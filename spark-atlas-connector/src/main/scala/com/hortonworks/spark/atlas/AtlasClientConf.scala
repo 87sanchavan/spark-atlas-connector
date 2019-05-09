421e8115eda0faa0ec355672c01eea7fc24b5c2b
87sanchavan <87sanchavan@gmail.com>
2019-05-09 18:25:56 +0530

    Update AtlasClientConf.scala

--- spark-atlas-connector/src/main/scala/com/hortonworks/spark/atlas/AtlasClientConf.scala
+++ spark-atlas-connector/src/main/scala/com/hortonworks/spark/atlas/AtlasClientConf.scala
@@ -17,11 +17,14 @@
 
 package com.hortonworks.spark.atlas
 
-import org.apache.atlas.ApplicationProperties
-import com.hortonworks.spark.atlas.AtlasClientConf.ConfigEntry
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 import java.io.{InputStream,FileInputStream}
 import java.util.Properties
 import java.util.Base64
+
+import org.apache.atlas.ApplicationProperties
+import com.hortonworks.spark.atlas.AtlasClientConf.ConfigEntry
 
 class AtlasClientConf {
 
@@ -55,41 +58,33 @@ class AtlasClientConf {
 }
 
 object AtlasClientConf {
-  case class ConfigEntry(key: String, defaultValue: String)
-
-  val ATLAS_SPARK_ENABLED = ConfigEntry("atlas.spark.enabled", "true")
-  val ATLAS_SPARK_COLUMN_ENABLED = ConfigEntry("atlas.spark.column.enabled", "false")
-
-  val ATLAS_REST_ENDPOINT = ConfigEntry("atlas.rest.address", "localhost:21000")
-
-  val BLOCKING_QUEUE_CAPACITY = ConfigEntry("atlas.blockQueue.size", "10000")
-  val BLOCKING_QUEUE_PUT_TIMEOUT = ConfigEntry("atlas.blockQueue.putTimeout.ms", "3000")
-
-
-val inputFile = new FileInputStream("/usr/hdp/2.6.5.0-292/spark2/conf/atlas-application.properties")
-val properties = new Properties
-properties.load(inputFile)
+ case class ConfigEntry(key: String, defaultValue: String)
+val hdfsConfiguration = new Configuration()
 val encoder=Base64.getEncoder
 val decoder=Base64.getDecoder
-val user = properties.getProperty("atlas.client.username.code")
-val pass = properties.getProperty("atlas.client.password.code")
+val hdfsFileSystem = FileSystem.get(hdfsConfiguration)
+val fsInputStream = hdfsFileSystem.open(new Path("/user/svc-dsat-adsadmin-np/atlas/file.properties"))
+val properties = new Properties()
+properties.load(fsInputStream)
+
+ val ATLAS_REST_ENDPOINT = ConfigEntry("atlas.rest.address", "localhost:21000")
+
+ val BLOCKING_QUEUE_CAPACITY = ConfigEntry("atlas.blockQueue.size", "10000")
+ val BLOCKING_QUEUE_PUT_TIMEOUT = ConfigEntry("atlas.blockQueue.putTimeout.ms", "3000")
+
+val user = properties.getProperty("atlas.client.username")
+val pass = properties.getProperty("atlas.client.password")
 val client = properties.getProperty("atlas.client.type")
 val decoded_user=decoder.decode(user).map(_.toChar).mkString
 val decoded_pass=decoder.decode(pass).map(_.toChar).mkString
 
-  
-  //val CLIENT_TYPE = ConfigEntry("atlas.client.type", "kafka")
- // val CLIENT_USERNAME = ConfigEntry("atlas.client.username", "admin")
- // val CLIENT_PASSWORD = ConfigEntry("atlas.client.password", "admin123")
+val CLIENT_TYPE = ConfigEntry("atlas.client.type", client)
+val CLIENT_USERNAME = ConfigEntry("atlas.client.username", decoded_user)
+val CLIENT_PASSWORD = ConfigEntry("atlas.client.password", decoded_pass)
 
-  val CLIENT_TYPE = ConfigEntry("atlas.client.type", client)
-  val CLIENT_USERNAME = ConfigEntry("atlas.client.username", decoded_user)
-  val CLIENT_PASSWORD = ConfigEntry("atlas.client.password", decoded_pass)
+ val CLIENT_NUM_RETRIES = ConfigEntry("atlas.client.numRetries", "3")
 
+ val CLUSTER_NAME = ConfigEntry("atlas.cluster.name", "primary")
 
-  val CLIENT_NUM_RETRIES = ConfigEntry("atlas.client.numRetries", "3")
-
-  val CLUSTER_NAME = ConfigEntry("atlas.cluster.name", "primary")
-
-  val CHECK_MODEL_IN_START = ConfigEntry("atlas.client.checkModelInStart", "true")
+ val CHECK_MODEL_IN_START = ConfigEntry("atlas.client.checkModelInStart", "true")
 }
